638d16fe97eb59a43ab088e3ff26c86e69531dbb
zeruFili <140791955+zeruFili@users.noreply.github.com>
2025-07-27 05:57:25 +0300

    add phone number field in  user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -27,6 +27,11 @@ const userSchema = new mongoose.Schema(
 			type: Boolean,
 			default: false,
 		},
+		phone_number: {
+			type: String,
+			required: true, 
+			unique: true,   
+		},
 		resetPasswordToken: String,
 		resetPasswordExpiresAt: Date,
 		verificationToken: String,
